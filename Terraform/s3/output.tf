b95807541dcac8258470b9facd72207a8794303d
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2023-08-19 11:07:15 +0900

    Update output.tf

--- Terraform/s3/output.tf
+++ Terraform/s3/output.tf
@@ -1,8 +1,10 @@
+# S3 bucket arn output 
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.project02-tf.arn
   description = "The ARN of the S3 bucket"
 }
 
+# Dynamodb name output
 output "dynamodb_table_name" {
   value       = aws_dynamodb_table.project02-dynamodb.name
   description = "The name of the DynamoDB table"
