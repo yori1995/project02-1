63b08e339a1d5077098f3c9c46117c76c335b576
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2023-08-19 11:19:22 +0900

    Update output.tf

--- Terraform/vpc/output.tf
+++ Terraform/vpc/output.tf
@@ -1,19 +1,24 @@
+# VPC ID OUTPUT
 output "vpc-id" {
   value = aws_vpc.project02-vpc.id
 }
 
+# PUBLIC SUBNET ID OUTPUT
 output "public-subnet2a" {
   value = aws_subnet.project02-subnet-public1.id
 }
 
+# PUBLIC SUBNET ID OUTPUT
 output "public-subnet2c" {
   value = aws_subnet.project02-subnet-public2.id
 }
 
+# PRIVATE SUBNET ID OUTPUT
 output "private-subnet2a" {
   value = aws_subnet.project02-subnet-private1.id
 }
 
+# PRIVATE SUBNET ID OUTPUT
 output "private-subnet2c" {
   value = aws_subnet.project02-subnet-private2.id
 }
