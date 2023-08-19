2f701f75e02669bc1590333493f75dc2b300913f
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2023-08-19 11:37:56 +0900

    Update resource.tf

--- Terraform/launch/resource.tf
+++ Terraform/launch/resource.tf
@@ -1,7 +1,9 @@
+# Provider
 provider "aws" {
   region = "ap-northeast-2"
 }
 
+# Launch Template 생성
 resource "aws_launch_template" "project02-launch" {
   name                   = "project02-launch-template"
   image_id               = "ami-0e3cecadac3ca9415"
@@ -10,6 +12,7 @@ resource "aws_launch_template" "project02-launch" {
   vpc_security_group_ids = [data.terraform_remote_state.project02-security.outputs.web-security, data.terraform_remote_state.project02-security.outputs.ssh-security]
   user_data              = base64encode(data.template_file.web_output.rendered)
 
+# IAM Profile 연결
   iam_instance_profile {
     name = aws_iam_instance_profile.jenkins-profile-launch.name
   }
@@ -19,6 +22,7 @@ resource "aws_launch_template" "project02-launch" {
   }
 }
 
+# IAM Profile 생성
 resource "aws_iam_instance_profile" "jenkins-profile-launch" {
   name = "jenkins-profile-launch"
   role = data.aws_iam_instance_profiles.project02-cd-role.role_name
