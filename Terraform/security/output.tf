bcc5ffefb142db0a17c7d28158c93d86ba0efef3
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2023-08-19 11:25:31 +0900

    Update output.tf

--- Terraform/security/output.tf
+++ Terraform/security/output.tf
@@ -1,11 +1,14 @@
+# SSH 보안그룹 Output
 output "ssh-security" {
   value = aws_security_group.project02-ssh.id
 }
 
+# WEB 보안그룹 Output
 output "web-security" {
   value = aws_security_group.project02-web.id
 }
 
+# ALB 보안그룹 Output
 output "alb-security" {
   value = aws_security_group.project02-alb.id
 }
