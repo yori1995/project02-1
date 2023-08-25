e7e30dc8dc9da100a50f6b112df9ca45dab6c3b0
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2023-08-25 11:58:46 +0900

    Update jenkins.tf

--- Terraform/ec2/jenkins.tf
+++ Terraform/ec2/jenkins.tf
@@ -1,9 +1,3 @@
-# IAM Profile 생성
-resource "aws_iam_instance_profile" "jenkins-profile" {
-  name = "jenkins-profile"
-  role = data.aws_iam_instance_profiles.project02-role-ec2.role_name
-}
-
 # Jenkins Instance 생성
 resource "aws_instance" "project02-jenkins" {
   ami                         = data.aws_ami.ubuntu.image_id
@@ -12,7 +6,6 @@ resource "aws_instance" "project02-jenkins" {
   vpc_security_group_ids      = [data.terraform_remote_state.project02-security.outputs.web-security, data.terraform_remote_state.project02-security.outputs.ssh-security]
   subnet_id                   = data.terraform_remote_state.project02-vpc.outputs.private-subnet2a
   associate_public_ip_address = false
-  iam_instance_profile        = aws_iam_instance_profile.jenkins-profile.name
 
   tags = {
     Name = "project02-jenkins-instance"
